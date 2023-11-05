2057711960f04fa9416afbbaeb08d25da2473138
Quang <20378quang@gmail.com>
2023-11-04 22:42:05 -0400

    Update ball_puzzle.py

--- Labs/Lab 8 - Ball Puzzle/ball_puzzle.py
+++ Labs/Lab 8 - Ball Puzzle/ball_puzzle.py
@@ -4,6 +4,8 @@ file: ball_puzzle.py
 language: python3
 author: Quang Huynh
 """
+
+
 import ball_puzzle_animate as bsa
 import cs_stack as s
 from dataclasses import dataclass
@@ -11,10 +13,16 @@ from dataclasses import dataclass
 
 @dataclass
 class Cans:
-    size: int
-    R: str
-    G: str
-    B: str
+    red: list
+    green: list
+    blue: list
+
+
+red = s.mk_empty_stack()
+green = s.mk_empty_stack()
+blue = s.mk_empty_stack()
+
+cans = [red, green, blue]
 
 
 def move_ball(start_can, end_can):
@@ -26,13 +34,30 @@ def move_ball(start_can, end_can):
         s.push(end_can, top_of_can)
 
 
+def algorithm(initial_can):
+    moves = 0
+
+    while red or green or blue:
+        top_of_can = s.top(initial_can)
+        if top_of_can == "R":
+            if not green:
+                move_ball("red", "blue")
+            elif not blue:
+                move_ball("red", "green")
+            else:
+                move_ball("red", "green")
+        elif top_of_can == "G":
+            move_ball("green", "red")
+        elif top_of_can == "B":
+            move_ball("blue", "red")
+        moves += 1
+    return moves
+
 def main():
-    initial_red_can = input("Enter the initial configuration in the 'red' can: ")
-    green_can = []
-    blue_can = []
-    bsa.animate_init(initial_red_can)
-    moves_required = move_ball(initial_red_can, end_can)
-    print("Puzzle solved in " + moves_required + "!")
+    initial_can = input("Enter the initial configuration in the 'red' can: ")
+    bsa.animate_init(initial_can)
+    number_of_moves = algorithm(initial_can)
+    print("Puzzle solved in" + str(number_of_moves) + "!")
     input("Close the window to quit.")
     bsa.animate_finish()
 
