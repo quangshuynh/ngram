c005eb3545e0ee5e537cb2f1f98e28447c1523c1
Quang <20378quang@gmail.com>
2023-11-14 08:20:15 -0500

    Update dna.py

--- Labs/Lab 9 - DNA Toolkit/dna.py
+++ Labs/Lab 9 - DNA Toolkit/dna.py
@@ -8,26 +8,51 @@ author: Quang Huynh
 import immutable_extra as ie
 import node_types as nt
 
+
 def convert_to_nodes(dna_string):
+    """
+    Converts a string of DNA into one node in a sequence
+    :param dna_string: Inputted string of DNA
+    :return: A linked-node representing the inputted DNA sequence
+    """
     if len(dna_string) == 0:
         return None
     else:
         node = nt.FrozenNode(dna_string[0], convert_to_nodes(dna_string[1:]))
     return node
 
+
 def convert_to_string(dna_seq):
-    if not dna_seq:
+    """
+    Converts linked lists of DNA into string using recursion
+    :param dna_seq: A linked sequence of DNA nodes
+    :return: The string value
+    """
+    if dna_seq is None:
         return ""
     else:
         return dna_seq.value + convert_to_string(dna_seq.next)
 
+
 def length_rec(dna_seq):
+    """
+    Finds the length of a linked node using recursion
+    :param dna_seq: A linked sequence of DNA nodes
+    :return: The length of a linked node
+    """
     if not dna_seq:
         return 0
     else:
         return 1 + length_rec(dna_seq.next)
 
+
 def is_match(dna_seq1, dna_seq2):
+    """
+    Checks if two sequences are equal to each other
+    :param dna_seq1: The first linked sequence of DNA
+    :param dna_seq2: The second linked sequence of DNA
+    :return: A boolean that states whether the DNA sequences match or not
+    """
     if dna_seq1 is None and dna_seq2 is None:
         return True
     elif dna_seq1 is None or dna_seq2 is None:
@@ -37,7 +62,14 @@ def is_match(dna_seq1, dna_seq2):
     else:
         return is_match(dna_seq1.next, dna_seq2.next)
 
+
 def is_pairing(dna_seq1, dna_seq2):
+    """
+    Checks and determines if two DNA sequences form a valid pairing
+    :param dna_seq1: The first linked sequence of DNA
+    :param dna_seq2: The second linked sequence of DNA
+    :return: True if sequences form a valid pairing, else False
+    """
     if dna_seq1 is None and dna_seq2 is None:
         return True
     elif dna_seq1 is None or dna_seq2 is None:
@@ -54,21 +86,47 @@ def is_pairing(dna_seq1, dna_seq2):
         else:
             return False
 
+
 def substitute(dna_seq1, idx, base):
-    pass
+    """
+    Performs a substitution mutation on a DNA sequence.
+    :param dna_seq1: The original linked list of a DNA sequence
+    :param idx: Index of the substitution
+    :param base: The new linked list of a DNA sequence
+    :return: The linked sequence after the substitution mutation
+    :raise: IndexError if idx is out of range.
+    """
+    dna_seq1_length = length_rec(dna_seq1)
+    if idx < 0 or idx > dna_seq1_length:
+        raise IndexError("Invalid substitution index.")
+    removed = ie.remove_at(idx, dna_seq1)  # Remove the element at the specified index
+    substituted = ie.concatenate(removed, nt.FrozenNode(base, None))  # Concatenate the new base at the specified index
+    return substituted
+
 
 def insert_seq(dna_seq1, dna_seq2, idx):
-    if idx > 0 or dna_seq1 is None:
+    """
+        Inserts the entire dna_seq2 before the specified index in dna_seq1
+        :param dna_seq1: The first linked sequence of DNA for insertion mutation
+        :param dna_seq2: The second linked sequence of DNA to be inserted
+        :param idx: Index for insertion in dna_seq1
+        :return: The linked sequence after the insertion
+        :raise: IndexError if idx is out of range.
+        """
+    if idx < 0:
         raise IndexError("Invalid insertion index")
-    elif idx == 0:
-        return ie.concatenate(dna_seq1, dna_seq2)
+    if idx == 0:
+        return ie.concatenate(dna_seq2, dna_seq1)  # Insert at the beginning
+    elif dna_seq1 is None and idx > 0:
+        raise IndexError("Invalid insertion index")
     else:
-        insert_next = insert_seq(dna_seq1.next, dna_seq2, idx - 1)
+        insert_next = insert_seq(dna_seq1.next, dna_seq2, idx - 1)  # Recursively insert into the rest of the sequence
         return nt.FrozenNode(dna_seq1.value, insert_next)
 
 
 def delete_seq(dna_seq, idx, segment_size):
     pass
+
 
 def duplicate_seq(dna_seq, idx, segment_size):
     pass
