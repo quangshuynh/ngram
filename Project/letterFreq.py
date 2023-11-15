7ab4555a9d6bfc9afbdf4033a23aec6fad41ac8a
Quang <20378quang@gmail.com>
2023-11-15 10:28:52 -0500

    Update letterFreq.py

--- Project/letterFreq.py
+++ Project/letterFreq.py
@@ -9,17 +9,19 @@ import matplotlib.pyplot as plt
 
 def letterFreq(words):
     """
-    Computes relative frequency of English characters occurring in print
+    Computes relative frequency of English characters occurring in print  & plot character counts.
 
-    :param words: Dictionary mapping words to dictionaries with years and counts
+    :param words: List of words representing the text for character frequency analysis
 
     :return: String containing the 26 lowercase characters in the English alphabet, sorted in
     decreasing order of frequency of occurrence of each character.
     """
-    letters = "".join([word.lower() for word in words])  # Extract all letters from words
-    letter_counts = {letter: letters.count(letter) for letter in set(letters)}  # Count occurrence of each letter
+    letters = "".join(word.lower() for word in words)
+    letter_counts = {}
+    for letter in list(letters):  # Count occurrence of each letter
+        letter_counts[letter] = letters.count(letter)
     sorted_letters = sorted(letter_counts, key=letter_counts.get, reverse=True)  # In descending order, sort letters by frequency
-    plt.bar(list(letters), list(letter_counts), color="skyblue")  # Plot character counts
+    plt.bar(list(sorted_letters), list(letter_counts), color="skyblue")  # Plot character counts
     plt.show()
     return "".join(sorted_letters)
 
