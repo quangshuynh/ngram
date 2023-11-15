7b17217cf624839e9059326ea5776ea1e9b7255c
Quang <20378quang@gmail.com>
2023-11-14 20:38:09 -0500

    Update wordSimilarity.py

--- Project/wordSimilarity.py
+++ Project/wordSimilarity.py
@@ -1,1 +1,8 @@
-
+"""
+Task 4: Program that calculates word similarity based on vector representation of word counts over
+multiple years. Each word is treated as a vector in a space defined by the years, with missing counts
+represented as zeros. The cosine of the angle between two word vectors is used as the measure of similarity.
+file: wordSimilarity.py
+language: python3
+author: Quang Huynh
+"""
