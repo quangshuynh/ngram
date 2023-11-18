fa36f13fddc03dc8c516fcc2982b3b49005bfea5
Quang <20378quang@gmail.com>
2023-11-17 19:10:35 -0500

    Update printedWords.py

--- Project/printedWords.py
+++ Project/printedWords.py
@@ -4,3 +4,30 @@ file: printedWords.py
 language: python3
 author: Quang Huynh
 """
+
+import matplotlib.pyplot as plt
+
+def printedWords(words):
+    """
+    Calculate the total word count for each year with available data
+    :param words: List of words representing the text for character frequency analysis
+    :return:
+    """
+    unique = set()  # Initialize set to store unique years
+    for word, data in words.items():  # Iterate through input to grab unique years
+        unique.update(data.keys())
+    sorted_years = sorted(list(unique))  # Convert unique years set into a sorted list
+    result = []  # Initialize empty list to store result
+    for year in sorted_years:  # Iterate through sorted years and find total count for each year
+        total_count = sum(data.get(year, 0) for data in words.values())
+        result.append((year, total_count))
+    return result
+
+def wordsForYear(year, yearList):
+    """
+    pass
+    :param year:
+    :param yearList:
+    :return:
+    """
+
