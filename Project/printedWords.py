4c04831c1f9fbc0088cc9895f9a6b40b3817235d
Quang <20378quang@gmail.com>
2023-11-27 09:25:27 -0500

    Update printedWords.py

--- Project/printedWords.py
+++ Project/printedWords.py
@@ -11,23 +11,30 @@ def printedWords(words):
     """
     Calculate the total word count for each year with available data
     :param words: List of words representing the text for character frequency analysis
-    :return:
+    :return: A sorted list containing a year and its corresponding total word count
     """
-    unique = set()  # Initialize set to store unique years
-    for word, data in words.items():  # Iterate through input to grab unique years
-        unique.update(data.keys())
-    sorted_years = sorted(list(unique))  # Convert unique years set into a sorted list
-    result = []  # Initialize empty list to store result
-    for year in sorted_years:  # Iterate through sorted years and find total count for each year
-        total_count = sum(data.get(year, 0) for data in words.values())
-        result.append((year, total_count))
-    return result
+    year_counts = {}  # Initialize empty dictionary
+    for word, data in words.items():  # Iterate through input dictionary of words & data
+        for year, count in data.items():  # Iterate through data dictionary for each word
+            if year in year_counts:  # Checks if year is in year_counts
+                year_counts[year] += count  # Add word count to current year total
+            else:  # If year is not in year_counts
+                year_counts[year] = count  # Initialize total word count for year
+    x_axis = sorted(year_counts.keys())  # Years for x-axis
+    plt.plot(x_axis, [year_counts[year] for year in x_axis])  # Plot total printed words for each year
+    plt.show()
+    total_words = sorted(year_counts.items())  # Sorted word counts
+    return total_words
 
 def wordsForYear(year, yearList):
     """
-    pass
-    :param year:
-    :param yearList:
-    :return:
+    Counts words for a specific year from list of year-count pairs
+    :param year: Target year for which desired word count
+    :param yearList: List of year-count pairs.
+    :return: Count of words for specified year. Returns 0 if year is not found in the list
     """
+    for current_year, count in yearList:  # Iterate through each year-count pair in list
+        if current_year == year:  # Checks if current year is equal to target year
+            return count
+    return 0  # Return 0 if target year is not found in list
 
