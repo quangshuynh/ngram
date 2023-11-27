ca67cab13c0f9b52caec1ebc51a8a8edf17def60
Quang <20378quang@gmail.com>
2023-11-27 09:26:10 -0500

    Update trending.py

--- Project/trending.py
+++ Project/trending.py
@@ -1,6 +1,18 @@
 """
-Task 3: Program that computes the top and bottom trending words between a given starting and ending year.
+Task 3: Program that computes the top and bottom trending words given starting & ending year.
 file: trending.py
 language: python3
 author: Quang Huynh
 """
+
+def trending(words, startYr, endYr):
+    """
+    Computes the top & bottom trending words between a starting & ending year
+    :param words:
+    :param startYr:
+    :param endYr:
+    :return:
+    """
+    top_trending = {}
+    bottom_trending = {}
+
