a2babf32dff2afb664d6711202e6e6303f1fd356
Quang <20378quang@gmail.com>
2023-11-28 10:04:46 -0500

    Update wordSimilarity.py

--- Project/wordSimilarity.py
+++ Project/wordSimilarity.py
@@ -7,61 +7,53 @@ language: python3
 author: Quang Huynh
 """
 
-import math
+import numpy as np
+
 
 def normalize(vector):
     """
     Normalize the given vector
-    :param vector:
-    :return:
+    :param vector: Input vector to be normalized
+    :return: Normalized vector
     """
-    vector_length = math.sqrt(sum(i ** 2 for i in vector))
-    if vector_length != 0:
-        normalized_vector = [i / vector_length for i in vector]
+    length = np.linalg.norm(vector)  # Calculate length of vector
+    if length != 0:  # Check if length is not 0 to avoid dividing by 0
+        return vector / length
     else:
-        normalized_vector = vector
-    return normalized_vector
+        return vector  # If length is 0, return original vector
 
-def cosine_similarity(vector1, vector2):
+
+def cos_similarity(vector1, vector2):
     """
     Calculate the cosine similarity between two vectors
-    :param vector1:
-    :param vector2:
-    :return:
+    :param vector1: First vector
+    :param vector2: Second vector
+    :return: Cosine similarity between vector1 & vector2
     """
-    dot_product = sum(vector1[i] * vector2[i] for i in range(len(vector1)))
-    return dot_product
-
-
-def pad_vector(vector, length):
-    """
-    Pad the vector with zeros to the specified length
-    :param vector:
-    :param length:
-    :return:
-    """
-    return vector + [0] * (length - len(vector))
+    return np.dot(vector1, vector2)
 
 
 def topSimilar(words, word):
     """
     Calculate the top 5 most similar words to the given word based on cosine similarity
     :param words: A dictionary mapping words to dictionaries with years and counts
-    :param word: A word for which we are looking for similar words
-    :return: A list of the top five words, including the input word, in descending order of similarity
+    :param word: Input word to find similarities to
+    :return: List of the top five words, including the input word, in descending order of similarity
     """
-    if word not in words:
-        return [word]
-    word_vector = list(words[word].values())
-    max_length = max(len(word_vector), max(len(v) for v in words.values()))
-    normalized_word_vector = normalize(pad_vector(word_vector, max_length))
-    similarities = []
-    for other_word, other_data in words.items():
-        if other_word != word:
-            other_vector = list(other_data.values())
-            normalized_other_vector = normalize(pad_vector(other_vector, max_length))
-            similarity = cosine_similarity(normalized_word_vector, normalized_other_vector)
-            similarities.append((other_word, similarity))
-    similarities.sort(key=lambda x: x[1], reverse=True)
-    top_similar_words = [word] + [item[0] for item in similarities[:4]]
-    return top_similar_words
+    if word not in words:  # Check if the word is in the dictionary
+        return [word]  # Returns a list of the input word if not found
+    years = set()  # Initialize set to store unique years for all words
+    for input_word, years1 in words.items():  # Iterate through each word and its corresponding years
+        years.update(years1.keys())  # Update the years set
+    word_vector = np.array([words[word].get(year, 0) for year in sorted(years)])
+    normalized = normalize(word_vector)  # Normalize input word vector
+    similar_words = {}  # Initialize empty dictionary to store input word & similar words
+    for current_word, years2 in words.items():
+        vector2 = np.array([years2.get(year, 0) for year in sorted(years)])
+        normalized2 = normalize(vector2)  # Normalize current word vector
+        similarity = cos_similarity(normalized, normalized2)  # Calculate cosine similarity between input word & current word
+        similar_words[current_word] = similarity  # Store similarity with current word as the key
+    top_similar_words = sorted(similar_words, key=similar_words.get, reverse=True)[:5]  # Ends at the fifth word
+    return top_similar_words  # Returns top five similar words in descending order
+
+
