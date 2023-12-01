dd9292c56d06affaced7d84454e73e37f6d0b43e
Quang <20378quang@gmail.com>
2023-12-01 12:39:35 -0500

    Update wordSimilarity.py

--- Project/wordSimilarity.py
+++ Project/wordSimilarity.py
@@ -8,11 +8,12 @@ author: Quang Huynh
 """
 
 import numpy as np
-
+import wordData as wd
+import time
 
 def normalize(vector):
     """
-    Normalize the given vector
+    Normalize given vector
     :param vector: Input vector to be normalized
     :return: Normalized vector
     """
@@ -30,7 +31,7 @@ def cos_similarity(vector1, vector2):
     :param vector2: Second vector
     :return: Cosine similarity between vector1 & vector2
     """
-    return np.dot(vector1, vector2)
+    return np.dot(vector1, vector2)  # Dot product of vectors
 
 
 def topSimilar(words, word):
@@ -41,14 +42,14 @@ def topSimilar(words, word):
     :return: List of the top five words, including the input word, in descending order of similarity
     """
     if word not in words:  # Check if the word is in the dictionary
-        return [word]  # Returns a list of the input word if not found
-    years = set()  # Initialize set to store unique years for all words
-    for input_word, years1 in words.items():  # Iterate through each word and its corresponding years
+        return [word]  # Returns input word if not found
+    years = set()  # Initialize set to store unique years for all words / no duplicates
+    for input_word, years1 in words.items():  # Iterate through & return input word and its corresponding years
         years.update(years1.keys())  # Update the years set
     word_vector = np.array([words[word].get(year, 0) for year in sorted(years)])
     normalized = normalize(word_vector)  # Normalize input word vector
     similar_words = {}  # Initialize empty dictionary to store input word & similar words
-    for current_word, years2 in words.items():
+    for current_word, years2 in words.items():  # Iterate through & return current word and its corresponding years
         vector2 = np.array([years2.get(year, 0) for year in sorted(years)])
         normalized2 = normalize(vector2)  # Normalize current word vector
         similarity = cos_similarity(normalized, normalized2)  # Calculate cosine similarity between input word & current word
@@ -56,34 +57,14 @@ def topSimilar(words, word):
     top_similar_words = sorted(similar_words, key=similar_words.get, reverse=True)[:5]  # Ends at the fifth word
     return top_similar_words  # Returns top five similar words in descending order
 
-# Testing time
+
+# Standalone execution
+def main():
+    file = input("Enter word file: ")
+    words = wd.readWordFile(file)
+    word = input("Enter word: ")
+    print("The most similar words are: \n" + str(topSimilar(words, word)))
+
 
 if __name__ == "__main__":
-    import time
-    import wordData as wd
-
-
-    def test(result, expected):
-        if result == expected:
-            print("Success")
-            return True
-        else:
-            print("Result: " + str(result) + "\nExpected" + str(expected))
-            return False
-
-    def testFile(filename, word, expected):
-        file = wd.readWordFile(filename)
-        return test(topSimilar(file, word), expected)
-
-    def testing():
-        start = time.perf_counter()
-        passed = True
-        passed = testFile("all.txt", "robot", ["robot", "robots", "robotics", "neuroendocrine", "programmable"]) and passed
-        print()
-        result = "all.txt success" if passed else "all.txt failed"
-        print(str(result))
-        print(time.perf_counter() - start)
-
-    testing()
-
-
+    main()
