3cd5c2d56a49293b89dc4fb1d256938faf31b87d
Quang <20378quang@gmail.com>
2023-12-01 12:37:10 -0500

    Update trending.py

--- Project/trending.py
+++ Project/trending.py
@@ -15,15 +15,15 @@ def trending(words, startYr, endYr):
     :param endYr: End year for calculating trending words
     :return: A list of tuples containing the word and its trend value
     """
-    trending_words = []  # Initialize empty list
+    trending_list = []  # Initialize empty list
     for word, counts in words.items():  # Iterate through & return input dictionary of words & counts
         starting_count = counts.get(startYr, 0)  # Get count for starting year, set to 0 if not present
         end_count = counts.get(endYr, 0)  # Get count for end year, set to 0 if not present
         if starting_count >= 1000 and end_count >= 1000:  # Check if the word meets the requirements
             trend_value = end_count / starting_count  # Calculate trend value
-            trending_words.append((word, trend_value))  # Append word & its trend value to list
-    trending_words.sort(key=lambda x: x[1], reverse=True)  # Sort the list in decreasing order of trend value
-    return trending_words
+            trending_list.append((word, trend_value))  # Append word & its trend value to list
+    trending_list.sort(key=lambda x: x[1], reverse=True)  # Sort the list in decreasing order of trend value
+    return trending_list
 
 
 # Standalone execution
@@ -34,7 +34,11 @@ def main():
     end = input("Enter ending year: ")
     trending_words = trending(words, start, end)
     print("The top 10 trending words from " + str(start) + " to " + str(end) + ": ")
-    
+    for idx in range(min(10, len(trending_words))):
+        print(trending_words[idx])
+    print("The bottom 10 trending words from " + str(start) + " to " + str(end) + ": ")
+    for idx in range(max(-10, -len(trending_words)), 0):
+        print(trending_words[idx])
 
 
 if __name__ == "__main__":
